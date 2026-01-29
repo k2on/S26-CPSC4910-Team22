914223269386f0460fcbd073c365f771582bb471
Max Koon <22125083+k2on@users.noreply.github.com>
2026-01-29 16:54:16 -0500

    feat: add convex

---
+++ convex/myFunctions.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { query, mutation, action } from "./_generated/server";
+import { api } from "./_generated/api";
+
+// Write your Convex functions in any file inside this directory (`convex`).
+// See https://docs.convex.dev/functions for more.
+
+export const getNumbers = query({
+  handler: async (ctx) => {
+    return {
+      numbers: await ctx.db.query("numbers").collect()
+    }
+  }
+})
+
+export const addNumber = mutation({
+  args: {
+    number: v.number()
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("numbers", { number: args.number });
+    return "ok";
+  }
+})
+
