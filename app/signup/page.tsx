b272614703a87c09660e045eff6629920d08fb75
Max Koon <22125083+k2on@users.noreply.github.com>
2026-02-23 13:59:12 -0500

    feat: add error message for emails already in use

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,13 +4,14 @@ import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Field, FieldDescription, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
 import Link from "next/link";
 import { useForm } from "@tanstack/react-form";
 import { useMutation } from "@tanstack/react-query";
 import { BetterFetchError } from "@better-fetch/fetch";
 import { parseFieldErrors } from "@/utils/parseFieldErrors";
 import { Spinner } from "@/components/ui/spinner";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 
 export default function Page() {
     const router = useRouter();
@@ -41,128 +42,152 @@ export default function Page() {
                         fields: parseFieldErrors(error.error.message)
                     }
                 })
+            } else if (error.error.code == "USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL") {
+                form.setErrorMap({
+                    onSubmit: {
+                        fields: {
+                            email: { message: error.error.message }
+                        }
+                    }
+                })
             }
         }
 
     })
 
     return (
-        <main className="max-w-lg mx-auto">
-            <label className="block text-3xl font-semibold text-center mb-4">
-                Create an account
-            </label>
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <div className="flex flex-col gap-6">
 
-            <form
-                className="flex flex-col"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    form.handleSubmit();
-                }}
-            >
-                <FieldGroup>
-                    <form.Field
-                        name="email"
-                        children={(field) => {
-                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
-                            return (
-                                <Field data-invalid={isInvalid}>
-                                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
-                                    <Input
-                                        id={field.name}
-                                        name={field.name}
-                                        value={field.state.value}
-                                        onBlur={field.handleBlur}
-                                        onChange={(e) => field.handleChange(e.target.value)}
-                                        aria-invalid={isInvalid}
-                                        disabled={isPending}
-                                        placeholder="Email"
+
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Create an account</CardTitle>
+                            <CardDescription>
+                                Enter your information below to create your account
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+
+
+                            <form
+                                className="flex flex-col"
+                                onSubmit={(e) => {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    form.handleSubmit();
+                                }}
+                            >
+                                <FieldGroup>
+                                    <form.Field
+                                        name="email"
+                                        children={(field) => {
+                                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                                            return (
+                                                <Field data-invalid={isInvalid}>
+                                                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                                                    <Input
+                                                        id={field.name}
+                                                        name={field.name}
+                                                        value={field.state.value}
+                                                        onBlur={field.handleBlur}
+                                                        onChange={(e) => field.handleChange(e.target.value)}
+                                                        aria-invalid={isInvalid}
+                                                        disabled={isPending}
+                                                        placeholder="Email"
+                                                    />
+                                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                                </Field>
+                                            )
+                                        }}
                                     />
-                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
-                                </Field>
-                            )
-                        }}
-                    />
-                    <form.Field
-                        name="password"
-                        children={(field) => {
-                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
-                            return (
-                                <Field data-invalid={isInvalid}>
-                                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
-                                    <Input
-                                        id={field.name}
-                                        name={field.name}
-                                        value={field.state.value}
-                                        onBlur={field.handleBlur}
-                                        onChange={(e) => field.handleChange(e.target.value)}
-                                        aria-invalid={isInvalid}
-                                        disabled={isPending}
-                                        type="password"
-                                        placeholder="Password"
+                                    <form.Field
+                                        name="password"
+                                        children={(field) => {
+                                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                                            return (
+                                                <Field data-invalid={isInvalid}>
+                                                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                                                    <Input
+                                                        id={field.name}
+                                                        name={field.name}
+                                                        value={field.state.value}
+                                                        onBlur={field.handleBlur}
+                                                        onChange={(e) => field.handleChange(e.target.value)}
+                                                        aria-invalid={isInvalid}
+                                                        disabled={isPending}
+                                                        type="password"
+                                                        placeholder="Password"
+                                                    />
+                                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                                </Field>
+                                            )
+                                        }}
                                     />
-                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
-                                </Field>
-                            )
-                        }}
-                    />
 
-                    <form.Field
-                        name="name"
-                        children={(field) => {
-                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
-                            return (
-                                <Field data-invalid={isInvalid}>
-                                    <FieldLabel htmlFor={field.name}>Name</FieldLabel>
-                                    <Input
-                                        id={field.name}
-                                        name={field.name}
-                                        value={field.state.value}
-                                        onBlur={field.handleBlur}
-                                        onChange={(e) => field.handleChange(e.target.value)}
-                                        aria-invalid={isInvalid}
-                                        disabled={isPending}
-                                        placeholder="Name"
+                                    <form.Field
+                                        name="name"
+                                        children={(field) => {
+                                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                                            return (
+                                                <Field data-invalid={isInvalid}>
+                                                    <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                                                    <Input
+                                                        id={field.name}
+                                                        name={field.name}
+                                                        value={field.state.value}
+                                                        onBlur={field.handleBlur}
+                                                        onChange={(e) => field.handleChange(e.target.value)}
+                                                        aria-invalid={isInvalid}
+                                                        disabled={isPending}
+                                                        placeholder="Name"
+                                                    />
+                                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                                </Field>
+                                            )
+                                        }}
                                     />
-                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
-                                </Field>
-                            )
-                        }}
-                    />
 
-                    <form.Field
-                        name="address"
-                        children={(field) => {
-                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
-                            return (
-                                <Field data-invalid={isInvalid}>
-                                    <FieldLabel htmlFor={field.name}>Mailing Address</FieldLabel>
-                                    <Input
-                                        id={field.name}
-                                        name={field.name}
-                                        value={field.state.value}
-                                        onBlur={field.handleBlur}
-                                        onChange={(e) => field.handleChange(e.target.value)}
-                                        aria-invalid={isInvalid}
-                                        disabled={isPending}
-                                        placeholder="Mailing Address"
+                                    <form.Field
+                                        name="address"
+                                        children={(field) => {
+                                            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                                            return (
+                                                <Field data-invalid={isInvalid}>
+                                                    <FieldLabel htmlFor={field.name}>Mailing Address</FieldLabel>
+                                                    <Input
+                                                        id={field.name}
+                                                        name={field.name}
+                                                        value={field.state.value}
+                                                        onBlur={field.handleBlur}
+                                                        onChange={(e) => field.handleChange(e.target.value)}
+                                                        aria-invalid={isInvalid}
+                                                        disabled={isPending}
+                                                        placeholder="Mailing Address"
+                                                    />
+                                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                                                </Field>
+                                            )
+                                        }}
                                     />
-                                    {isInvalid && <FieldError errors={field.state.meta.errors} />}
-                                </Field>
-                            )
-                        }}
-                    />
 
-                </FieldGroup>
-
-                <Button type="submit" className="mt-4" disabled={isPending}>
-                    {isPending ? <Spinner /> : "Sign Up"}
-                </Button>
-
-                <Button asChild variant="link" disabled={isPending}>
-                    <Link href="/signin">Already Have an Account? Sign In</Link>
-                </Button>
-            </form>
-        </main>
+                                    <FieldGroup>
+                                        <Field>
+                                            <Button type="submit" className="mt-4" disabled={isPending}>
+                                                {isPending ? <Spinner /> : "Sign Up"}
+                                            </Button>
+                                            <FieldDescription className="px-6 text-center">
+                                                Already have an account? <Link href="/signin">Sign in</Link>
+                                            </FieldDescription>
+                                        </Field>
+                                    </FieldGroup>
+                                </FieldGroup>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </div>
     );
 }
