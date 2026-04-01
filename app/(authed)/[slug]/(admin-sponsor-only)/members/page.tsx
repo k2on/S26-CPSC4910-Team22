a46717ad0a680c723a2469b8b5a523e13b52a63d
ryank0427 <114929069+ryank0427@users.noreply.github.com>
2026-04-01 07:21:50 -0400

    resize org members chart to fit page

--- app/(authed)/[slug]/(admin-sponsor-only)/members/page.tsx
+++ app/(authed)/[slug]/(admin-sponsor-only)/members/page.tsx
@@ -3,7 +3,7 @@ import { OrganizationMembers } from "@/components/org/members/Members";
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
     return (
-        <div className="mx-auto max-w-lg pt-8">
+        <div className="pt-8">
             <OrganizationMembers slug={slug} />
         </div>
     )
